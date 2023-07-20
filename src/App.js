7a34c62fbb7fc8f71433b4267fdc794754e5a7f9
Uthman <uthmancod@gmail.com>
2023-07-20 18:13:33 +0100

    adding functionalities + sample UI design
    this commit contains the almost all the functionalities for the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Github_user_search from './component/Github_user_search';
+
+
+function App() {
+
+  return (
+    <div >
+      <Github_user_search />
+    </div>
+  )
+
+}
+
+export default App;
